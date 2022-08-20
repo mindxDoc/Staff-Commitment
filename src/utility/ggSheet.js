67f16771530e9475614fd0f2643064e26e705e5e
mindxDoc <doc@mindx.edu.vn>
2022-08-20 17:45:17 +0700

    secure ggf token

--- src/utility/ggSheet.js
+++ src/utility/ggSheet.js
@@ -1,8 +1,10 @@
 import {makePostRequest} from "./api";
 import 'dotenv/config'
 
+const formToken = process.env.REACT_APP_GOOGLE_FORM;
+
 export const postMessageToGoogle = async (data) => {
-    const endpoint = 'https://docs.google.com/forms/d/e/1FAIpQLSccUAKYWu3NqWTCQrh2FkofXcfcEBJu2dxxF88EYfJgNij6oA/formResponse';
+    const endpoint = `https://docs.google.com/forms/d/e/${formToken}/formResponse`;
 
     await makePostRequest(endpoint, data);
 }
